582b2fe3ad7b2bdbb793d430142afbe252cb2e02
Peter Matthews <pjdmatthews7@gmail.com>
2022-06-30 08:39:55 -0400

    Initial Commit

--- build/CMakeFiles/fourier.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for fourier.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/fourier.dir/src/fftHelper.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/fourier.dir/src/fftHelper.cpp.o: \
-  /Users/peter/Projects/fourier/src/fftHelper.cpp \
-  /Users/peter/Projects/fourier/src/fftHelper.h
